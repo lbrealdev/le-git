091f7f085086007213970673e044f2329122198d
lbrealdev <lbrealdeveloper@gmail.com>
2023-01-09 18:22:03 +0100

    fix: rename script

---
+++ graphql-api/get-branch-protection-rule.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+gh api graphql -f query='{
+    repository(owner:"owner", name:"repository") {
+        branchProtectionRules(first:100)  {
+            nodes {
+                allowsDeletions,
+                allowsForcePushes,
+                blocksCreations,
+                creator {
+                    login,
+                    url,
+                    resourcePath,
+                    avatarUrl
+                },
+                databaseId,
+                dismissesStaleReviews,
+                isAdminEnforced,
+                lockAllowsFetchAndMerge,
+                pattern,
+                pushAllowances(first:100) {
+                   nodes {
+                     actor
+                   },
+                },
+                repository {
+                    createdAt,
+                    description,
+                    isInOrganization,
+                    languages(first: 100) {
+                        nodes { name }
+                    },
+                    name,
+                    nameWithOwner,
+                    owner { login },
+                    primaryLanguage {
+                        name
+                    },
+                    url,
+                    updatedAt
+                },
+                requireLastPushApproval,
+                requiredApprovingReviewCount,
+                lockBranch
+            }
+        }
+    }
+}'
