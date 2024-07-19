a3b338260c6b0b76ffbd0ee6c7d190c8dc62fdcb
lbrealdev <lbrealdeveloper@gmail.com>
2024-07-19 14:00:12 +0200

    fix: add scripts

---
+++ github-api/graphql-api/scripts/poc/list-repos-org/list_all_repos_org_v2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: ./$(basename -- "$0") <org-login>"
+  exit 1
+fi
+
+GITHUB_ORG="$1"
+GITHUB_TOKEN="$GITHUB_AUTH_TOKEN"
+
+page_cursor="null"
+has_next_page=true
+
+while $has_next_page; do
+  if [ "$page_cursor" == "null" ]; then
+    cursor_query="null"
+  else
+    cursor_query="\"$page_cursor\""
+  fi
+
+  graphql_schema=$(echo "query {
+    organization(login: \"$GITHUB_ORG\") {
+        id
+        name
+        login
+        repositories(first: 100, after: $cursor_query) {
+            nodes {
+                id
+                name
+            }
+            pageInfo {
+                endCursor
+                hasNextPage
+            }
+        }
+    }
+  }" | tr -d '\n' | tr -s ' ' | sed 's/"/\\"/g')
+
+  query=$(curl -sL \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    https://api.github.com/graphql \
+    -d "{ \"query\": \"$graphql_schema\"}")
+
+  repos=$(echo "$query" | jq -r .data.organization.repositories.nodes[].name)
+  echo "$repos"
+
+  page_cursor=$(echo "$query" | jq -r .data.organization.repositories.pageInfo.endCursor)
+  has_next_page=$(echo "$query" | jq -r .data.organization.repositories.pageInfo.hasNextPage)
+
+  if [ "$has_next_page" == "false" ]; then
+    break
+  fi
+
+done
